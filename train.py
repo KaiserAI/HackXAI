532d2a39f5e3852bcd40e61800f1e1ac7c0d562b
Kaiser_AI <josewalter2468@gmail.com>
2024-05-27 16:56:39 +0200

    train con orientacion a objetos

--- train.py
+++ train.py
@@ -10,28 +10,16 @@ from sklearn.model_selection import train_test_split
 # ml_kx entrenado pa aprender el sexo
 # ml_ban
 
-# [] Dividir train y test antes de usos.
 # [] Probar con svm, árboles y modelos probabilísticos.
-
 class BinaryClassifier:
-    def __init__(self, model_type='linear'):
-        if model_type == 'linear':
-            self.model = linear_model.LinearRegression()
-        elif model_type == 'logistic':
-            self.model = linear_model.LogisticRegression(max_iter=10000000)  # 1000 10000000
-        else:
-            raise ValueError("Unsupported model_type. Use 'linear' or 'logistic'.")
-        self.model_type = model_type
+    def __init__(self, model):
+        self.model = model
 
     def fit(self, x, y):
         self.model.fit(x, y)
 
     def predict(self, x):
-        if self.model_type == 'linear':
-            predictions = self.model.predict(x)
-            return np.where(predictions > 0.5, 1, 0)
-        elif self.model_type == 'logistic':
-            return self.model.predict(x)
+        return self.model.predict(x)
 
     def save_model(self, file_path):
         with open(file_path, 'wb') as model_file:
@@ -42,62 +30,87 @@ class BinaryClassifier:
             self.model = pickle.load(model_file)
 
 
-def ml_kx_predict_and_to_dataframe(model_type, x_without_bias_category, bias_category_name, bias_category_pos):
-    loaded_reg_model_kx = BinaryClassifier()
-    if model_type == 'linear':
-        loaded_reg_model_kx.load_model("pickle/reg_model_ml_kx_linear.pkl")
-    elif model_type == 'logistic':
-        loaded_reg_model_kx.load_model("pickle/reg_model_ml_kx_logistic.pkl")
-    else:
-        raise ValueError("Unsupported model_type. Use 'linear' or 'logistic'.")
-    y_score_ml_kx = loaded_reg_model_kx.predict(x_without_bias_category)
+class LinearRegressionBinary(BinaryClassifier):
+    def __init__(self, model=None):
+        if model is None:
+            model = linear_model.LinearRegression()
+        super().__init__(model)
 
-    # Convierte a DataFrame
-    y_score_ml_kx = pd.DataFrame(y_score_ml_kx, columns=[bias_category_name])
-    # Restablece los índices de los DataFrames
-    x_without_bias_category.reset_index(drop=True, inplace=True)
-    y_score_ml_kx.reset_index(drop=True, inplace=True)
+    def predict(self, x):
+        predictions = super().predict(x)
+        return np.where(predictions > 0.5, 1, 0)
 
-    x_with_bias_category_inferred = pd.concat([x_without_bias_category.iloc[:, :bias_category_pos], y_score_ml_kx,
-                                               x_without_bias_category.iloc[:, bias_category_pos:]], axis=1)
-    return x_with_bias_category_inferred
+
+class BiasedMlx(LinearRegressionBinary):
+    def __init__(self, bias_category_name, bias_category_pos, missing_feature_model, final_prediction_model):
+        self.bias_category_name = bias_category_name
+        self.bias_category_position = bias_category_pos
+        self.missing_feature_model = missing_feature_model
+        super().__init__(final_prediction_model)
+
+    def predict(self, x):
+        # Predecir la categoría faltante
+        y_score_ml_kx = self.missing_feature_model.predict(x)
+
+        # Convertir a DataFrame
+        y_score_ml_kx = pd.DataFrame(y_score_ml_kx, columns=[self.bias_category_name])
+
+        # Restablece los índices de los DataFrames
+        x.reset_index(drop=True, inplace=True)
+        y_score_ml_kx.reset_index(drop=True, inplace=True)
+
+        # Concatenar la predicción con el DataFrame original
+        x_with_bias_category_inferred = pd.concat([x.iloc[:, :self.bias_category_position], y_score_ml_kx,
+                                                   x.iloc[:, self.bias_category_position:]], axis=1)
+
+        # Realizar la predicción final utilizando el nuevo DataFrame
+        predictions = super().predict(x_with_bias_category_inferred)
+        return predictions
 
 
 if __name__ == "__main__":
-    # data = pd.read_csv('data/german_data.csv', sep=',', decimal='.', index_col=0, na_values='null')
+    data = pd.read_csv('data/german_data.csv', sep=',', decimal='.', index_col=0, na_values='null')
+    bias_category_name = "sex"
+    target = "approval"
+    '''
     data = pd.read_csv('data/taiwan_data.csv', sep=',', decimal='.', index_col=0, na_values='null')
+    bias_category_name = "SEX"
+    target = "Creditworthiness"
+    '''
+    '''
+    data = pd.read_csv('data/taiwan_data_02.csv', sep=';', decimal='.', index_col=0, na_values='null')
+    bias_category_name = "PAY_0"  # "sex" "Age Group" "SEX"
+    target = "Creditworthiness"
+    '''
 
-    # bias category
-    bias_category = "SEX"  # "sex" "Age Group" "SEX"
-    bias_category_position = data.columns.get_loc(bias_category)
+    bias_category_position = data.columns.get_loc(bias_category_name)
 
-    # target
-    target = "Creditworthiness"  # "approval" "Creditworthiness"
+    # Particion:
+    seed = 289
+    train, test = train_test_split(data, test_size=0.2, random_state=seed)
 
-    # Separo el target de los datos de entreno.
-    y_data = data[target].copy()
-    x_data = data.drop(labels=target, axis=1).copy()
-    x_without_bias_category_data = data.drop(labels=[bias_category, target], axis=1).copy()
-    y_predict_bias_category_data = data[bias_category].copy()
-    x_predict_bias_category_data = data.drop(labels=[bias_category, target], axis=1).copy()
-
-    # Separo en train y test:
-    seed = 286
-    # Modelo ML_ox.
-    x_train, x_test = train_test_split(x_data, test_size=0.2, random_state=seed, stratify=y_data)
-    y_train, y_test = train_test_split(y_data, test_size=0.2, random_state=seed, stratify=y_data)
-    # Modelo ml_ban: entrenado sin la categoría bias.
-    x_without_bias_category_train, x_without_bias_category_test = train_test_split(x_without_bias_category_data,
-                                                                                   test_size=0.2, random_state=seed, stratify=y_data)
-    # Modelo ml_kx: predice la categoría bias.
-    x_predict_bias_category_train, x_predict_bias_category_test = train_test_split(x_predict_bias_category_data,
-                                                                                   test_size=0.2, random_state=seed, stratify=y_predict_bias_category_data)
-    y_predict_bias_category_train, y_predict_bias_category_test = train_test_split(y_predict_bias_category_data,
-                                                                                   test_size=0.2, random_state=seed, stratify=y_predict_bias_category_data)
+    y_train = train[target].copy()
+    y_test = test[target].copy()
+    x_train = train.drop(labels=target, axis=1).copy()
+    x_test = test.drop(labels=target, axis=1).copy()
+    x_without_bias_category_train = train.drop(labels=[bias_category_name, target], axis=1).copy()
+    x_without_bias_category_test = test.drop(labels=[bias_category_name, target], axis=1).copy()
+    y_predict_bias_category_train = train[bias_category_name].copy()
+    y_predict_bias_category_test = test[bias_category_name].copy()
+    x_predict_bias_category_train = train.drop(labels=[bias_category_name, target], axis=1).copy()
+    x_predict_bias_category_test = test.drop(labels=[bias_category_name, target], axis=1).copy()
 
     # Ordenar los df por id y guardarlos en csv:
-    # x_train.sort_index(inplace=True) # si quito estos 2 se va a la mierda todo.
-    # y_train.sort_index(inplace=True)
+    x_train.sort_index(inplace=True)
+    y_train.sort_index(inplace=True)
+    x_test.sort_index(inplace=True)
+    y_test.sort_index(inplace=True)
+    x_without_bias_category_train.sort_index(inplace=True)
+    x_without_bias_category_test.sort_index(inplace=True)
+    y_predict_bias_category_train.sort_index(inplace=True)
+    y_predict_bias_category_test.sort_index(inplace=True)
+    x_predict_bias_category_train.sort_index(inplace=True)
+    x_predict_bias_category_test.sort_index(inplace=True)
 
     x_test.to_csv('data/x_test.csv', index=True)
     y_test.to_csv('data/y_test.csv', index=True)
@@ -106,27 +119,18 @@ if __name__ == "__main__":
     y_predict_bias_category_test.to_csv('data/y_predict_bias_category_test.csv', index=True)
 
     # Entrenar los modelos de regresión lineal
-    reg_model_ml_ox_linear = BinaryClassifier(model_type='linear')
-    reg_model_ml_ox_linear.fit(x_train, y_train)
-    reg_model_ml_ox_linear.save_model("pickle/reg_model_ml_ox_linear.pkl")
+    reg_model_ml_ox = LinearRegressionBinary()
+    reg_model_ml_ox.fit(x_train, y_train)
+    reg_model_ml_ox.save_model("pickle/reg_model_ml_ox.pkl")
 
-    reg_model_ml_ban_linear = BinaryClassifier(model_type='linear')
-    reg_model_ml_ban_linear.fit(x_without_bias_category_train, y_train)
-    reg_model_ml_ban_linear.save_model("pickle/reg_model_ml_ban_linear.pkl")
+    reg_model_ml_ban = LinearRegressionBinary()
+    reg_model_ml_ban.fit(x_without_bias_category_train, y_train)
+    reg_model_ml_ban.save_model("pickle/reg_model_ml_ban.pkl")
 
-    reg_model_ml_kx_linear = BinaryClassifier(model_type='linear')
-    reg_model_ml_kx_linear.fit(x_predict_bias_category_train, y_predict_bias_category_train)
-    reg_model_ml_kx_linear.save_model("pickle/reg_model_ml_kx_linear.pkl")
+    reg_model_ml_kx = LinearRegressionBinary()
+    reg_model_ml_kx.fit(x_predict_bias_category_train, y_predict_bias_category_train)
+    reg_model_ml_kx.save_model("pickle/reg_model_ml_kx.pkl")
 
-    # Entrenar los modelos de regresión logística
-    reg_model_ml_ox_logistic = BinaryClassifier(model_type='logistic')
-    reg_model_ml_ox_logistic.fit(x_train, y_train)
-    reg_model_ml_ox_logistic.save_model("pickle/reg_model_ml_ox_logistic.pkl")
-
-    reg_model_ml_ban_logistic = BinaryClassifier(model_type='logistic')
-    reg_model_ml_ban_logistic.fit(x_without_bias_category_train, y_train)
-    reg_model_ml_ban_logistic.save_model("pickle/reg_model_ml_ban_logistic.pkl")
-
-    reg_model_ml_kx_logistic = BinaryClassifier(model_type='logistic')
-    reg_model_ml_kx_logistic.fit(x_predict_bias_category_train, y_predict_bias_category_train)
-    reg_model_ml_kx_logistic.save_model("pickle/reg_model_ml_kx_logistic.pkl")
+    # Crear el modelo biased_mlx:
+    reg_model_biased_mlx = BiasedMlx(bias_category_name, bias_category_position, reg_model_ml_kx, reg_model_ml_ox)
+    reg_model_biased_mlx.save_model("pickle/reg_model_biased_mlx")
